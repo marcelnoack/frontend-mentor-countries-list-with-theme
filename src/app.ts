65e6625eb98efe2b4ecee8cdda2cc0922d8456f8
Marcel Noack <marcel.noack.dev@gmail.com>
2020-04-17 14:07:24 +0200

    add initial page setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 // Integrating SASS into the Project - Option 2: as an ES6 import inside a JS/TS file
-import './scss/main.scss';
+// import './scss/main.scss';
 
-const foo: string = 'Bar!';
+// const foo: string = 'Bar!';
 
-const app = document.getElementById('app');
-if (app) {
-  app.innerHTML = foo;
-  app.classList.add('app');
-}
+// const app = document.getElementById('app');
+// if (app) {
+//   app.innerHTML = foo;
+//   app.classList.add('app');
+//   // document.documentElement.classList.add("transition");
+//   setTimeout(() => {
+//     console.log('dark now');
+//     document.documentElement.setAttribute('data-theme', 'dark');
+//   }, 1000);
+// }
